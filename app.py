f64a79748a29dccb6e8b3d4725e18860ebf33e67
Samuel <154820885+samuelthomas5@users.noreply.github.com>
2025-12-23 15:13:53 +0000

    add note submission form

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        note =request.form["note"]
+        print(note)
 
-if __name__ == '__main__':
+    return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
